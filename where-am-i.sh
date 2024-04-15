aee03512c25e266e1c9679937fff735f41b82027
zStrikke <zstrikke@gmail.com>
2024-04-15 17:18:51 +0200

    silly script to launch after docker compose to show data of the project, WIP

---
+++ where-am-i.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\033[40m############################################\033[m"
+echo -e "Frontend URL: \033[4mhttps://zbooks.zstrikke.local/\033[m"
+echo -e "API URL: \033[4mhttps://zbooks.zstrikke.local/api/\033[m"
+echo -e "\033[40m############################################\033[m"
+printf "[INFO] Processing info project: bip bup beep boop\033[0K\r"
+sleep 4
+printf "[INFO] Showing info\033[0K\r\n"
+echo -e "PHP: \033[42m8.2.18\033[m"
+echo -e "MySQL: \033[42m8.0.33\033[m"
+echo -e "NGINX: \033[42m1.25.1\033[m"
+echo -e "REDIS: \033[42m7.0.11\033[m"
+echo -e "Laravel: \033[42m10.48.7\033[m"
+echo -e "node: \033[42m20.12.2\033[m"
+echo -e "npm: \033[42m10.5.0\033[m"
+echo -e "Angular: \033[42m17.3.4\033[m"
+echo -e "Angular CLI: \033[42m17.3.4\033[m"
+echo "--------------------------------------------"
+echo -e "\033[44mHappy Coding!\033[m"
